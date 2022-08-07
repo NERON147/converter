9d95473cf51155f360d7d48269a8d16b59f45816
NERON147 <bezzubenkomaxim@gmail.com>
2022-08-07 21:04:40 +0300

    qq

---
+++ main.js
@@ -0,0 +1,9 @@
+const inputCost = document.querySelector('.bablo');
+
+const cleavePriceSetting = {
+    numeral: true,
+    numeralThousandsGroupStyle: 'thousand',
+    delimiter: ' ',
+}
+
+const cleaveCost = new Cleave(inputCost, cleavePriceSetting);
