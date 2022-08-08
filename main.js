77c3880d79c4747267f925e41eae77b64bee2556
NERON147 <bezzubenkomaxim@gmail.com>
2022-08-08 23:24:08 +0300

    qq

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 const inputCost = document.querySelector('.bablo');
+const elementUSD = document.querySelector('[data-value="USD"]');
+const elementEUR = document.querySelector('[data-value="EUR"]');
+const input = document.querySelector('#input')
+const result = document.querySelector('#result')
+const select = document.querySelector('#select')
+
 
 const cleavePriceSetting = {
     numeral: true,
@@ -7,5 +13,32 @@ const cleavePriceSetting = {
 }
 
 const cleaveCost = new Cleave(inputCost, cleavePriceSetting);
+
+
+const valUsd = function () {
+fetch('https://www.cbr-xml-daily.ru/daily_json.js')
+    .then(response => response.json())
+    .then(json => console.log(json.Valute.USD.Value))
+    console.log(json)
+    
+}
+const valEur = function () { 
+fetch('https://www.cbr-xml-daily.ru/daily_json.js')
+.then(response => response.json())
+.then(json => console.log( json.Valute.EUR.Value))
+}
+
+    console.log(valUsd());
+    valEur();
+
+
+input.oninput = function () {
+    console.log('huy');
+    result.value = input.value / a;
+}
+    
+
+    
+
 
 
