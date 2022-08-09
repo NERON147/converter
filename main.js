f6475de96dbfc2e1ec3913cee13aa0cde70edc38
NERON147 <bezzubenkomaxim@gmail.com>
2022-08-09 21:30:58 +0300

    qq

--- main.js
+++ main.js
@@ -5,10 +5,6 @@ const select = document.querySelector('#select')
 const rates = {}
 
 
-
-
-
-
 const cleavePriceSetting = {
     numeral: true,
     numeralThousandsGroupStyle: 'thousand',
@@ -16,6 +12,7 @@ const cleavePriceSetting = {
 }
 
 const cleaveCost = new Cleave(input, cleavePriceSetting);
+const cleaveCost2 = new Cleave(result, cleavePriceSetting);
 
 async function getData () {
     const response = await fetch('https://www.cbr-xml-daily.ru/daily_json.js');
@@ -33,7 +30,10 @@ getData ();
 
 input.oninput = function () {
         
-        result.value = (input.value.replace(/\s/g, '') / rates[select.value].Value).toFixed(2)
+    const sum = (+cleaveCost.getRawValue() / rates[select.value].Value).toFixed(2)
+    result.value = sum
+   
+        // result.value = (input.value.replace(/\s/g, '') / rates[select.value].Value).toFixed(2)
     };
     
 select.oninput = function () {
